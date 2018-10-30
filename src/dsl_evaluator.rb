2df3e9c5e61ee15d1018ffdd8ef3520d76798065
Vinicius Oyama <vinicius.oyama@gmail.com>
2018-10-30 00:00:55 -0300

    Created first DSL evaluation world

--- src/dsl_evaluator.rb
+++ src/dsl_evaluator.rb
@@ -1,7 +1,21 @@
 require_relative 'person'
 
 class DSLEvaluator
-  def self.evaluate
-    {}
+  class World
+    attr_reader :persons
+
+    def initialize
+      @persons = {}
+    end
+
+    def person(name)
+      @persons[name] = Person.new
+    end
+  end
+
+  def self.evaluate(&block)
+    world = World.new
+    world.instance_eval(&block)
+    world.persons
   end
 end
